d9a3b656cbff7aed0bf67401c330825dd8cb4021
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-06-09 20:43:00 +0200

    Initial Set up

---
+++ README.md
@@ -0,0 +1,11 @@
+# quarkus_app
+
+## Programación DAW Dual
+
+## Key-Concepts :dart: 
+- Nokia
+- Motorola
+- BlackBerry
+
+## UML
+![Custom UML](quarkus_app_UML.png)
---
+++ src/main/java/edu/poniperro/resources/ItemResource.java
@@ -0,0 +1,67 @@
+package edu.poniperro.resources;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import edu.poniperro.models.Item;
+import edu.poniperro.services.ItemService;
+
+@Path("/item")
+public class ItemResource {
+
+    @Inject
+    ItemService itemService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get() {
+        return Response.ok(itemService.get()).build();
+    }
+
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@PathParam("name") String name) {
+        Item item = itemService.get(name);
+        return item.getName().isEmpty() ? 
+            Response.status(Response.Status.NOT_FOUND).build():
+            Response.status(Response.Status.OK).entity(item).build();  
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(@Valid Item item) {
+        return Response.status(Status.CREATED).entity(itemService.create(item)).build();
+    }
+
+    @PUT
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(Item item) {
+        return Response.status(Status.OK).entity(itemService.update(item)).build();
+    }
+
+    @DELETE
+    @Transactional
+    @Path("{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("name") String name) {
+        itemService.delete(name);
+        return Response.ok().build();
+    }
+}
